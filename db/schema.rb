19367a8f4dd70ca28c5583d69a323bd26c460121
AlexFuro <afurones43@gmail.com>
2018-06-05 08:01:13 -0400

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604112838) do
+ActiveRecord::Schema.define(version: 20180604234405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180604112838) do
     t.index ["requested_id"], name: "index_friend_requests_on_requested_id"
     t.index ["requestor_id", "requested_id"], name: "index_friend_requests_on_requestor_id_and_requested_id", unique: true
     t.index ["requestor_id"], name: "index_friend_requests_on_requestor_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,17 @@ joe.sent_requests.create(requested_id: bama.id, accepted: false)
 
 dave.sent_requests.create(requested_id: julia.id)
 dave.sent_requests.create(requested_id: bama.id, accepted: true)
+dave.sent_requests.create(requested_id: joe.id,  accepted: true)
 
 julia.sent_requests.create(requested_id: joe.id)
 julia.sent_requests.create(requested_id: dave.id, accepted: false)
 julia.sent_requests.create(requested_id: maria.id, accepted: true)
+
+bama.sent_requests.create(requested_id: dave.id, accepted: true)
+
+maria.sent_requests.create(requested_id: julia.id, accepted: true)
+
+#create some posts
+joe.posts.create(content: "Yo this is a sentence joe")
+dave.posts.create(content: "Yo this is a sentence dave")
+julia.posts.create(content: "Yo this is a sentence julia")
